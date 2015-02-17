055c7548682fc8cb5e0beee9125961f3cf591edc
mynkov <mynkovas@mail.ru>
2015-02-17 23:13:40 +0300

    test

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        private static readonly object SyncObject = new object();
+
+        private static event Action Click;
+
+        static void Main()
+        {
+            Click += () => { Console.WriteLine("1"); };
+            Click += () => { throw new Exception();};
+            Click += () => { Console.WriteLine("3"); };
+
+            Click();
+
+            try
+            {
+                new Action(() => { throw new Exception(); }).BeginInvoke(null, null);
+            }
+            catch
+            {
+                Console.WriteLine("catch");
+            }
+
+            //lock (5)
+            //{
+            //    Console.WriteLine("lock");
+            //}
+
+            var value = "";
+
+            if (value == null)
+            {
+                lock (SyncObject)
+                {
+                    if (value == null)
+                    {
+                        // ...
+                    }
+                }
+            }
+
+            // throw ex; throw;
+        }
+
+
+    }
+
+    public class Program1
+    {
+        static void Main1()
+        {
+            var a = new A();
+            //a.Method();
+        }
+
+        public class A : IB, IC
+        {
+            void IB.Method()
+            {
+                Console.WriteLine("B");
+            }
+
+            void IC.Method()
+            {
+                Console.WriteLine("C");
+            }
+        }
+
+        public interface IB
+        {
+            void Method();
+        }
+
+        public interface IC
+        {
+            void Method();
+        }
+    }
+}
